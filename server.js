7b6a8b7b055f7211f5aece082b3f3d9a277f9e56
Nalla Sai Krishna Reddy <105940056+sakrin18072@users.noreply.github.com>
2023-04-24 11:04:38 +0530

    Server Updated

--- server.js
+++ server.js
@@ -9,6 +9,8 @@ import podRoute from './routes/podRoute.js';
 import { fileURLToPath } from 'url'
 dotenv.config();
 connectDB();
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 
 let app = express();
@@ -16,10 +18,10 @@ app.use(cors());
 app.use(express.json());
 app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname,'./client/build')))
-app.use("*",(request,response)=>{
-    response.sendFile(path.join(__dirname,'./client/build/index.html'))
-  })
 
 app.use('/api/v1/auth',authRoute);
 app.use('/api/v1/podcast',podRoute);
+app.use("*",(request,response)=>{
+    response.sendFile(path.join(__dirname,'./client/build/index.html'))
+  })
 app.listen(process.env.PORT || 8080,()=>{console.log("Server is listening on port 8080")});
